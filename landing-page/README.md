80b6a801b49c6d617115c9c076dc3328546d534f
imina-malak <91422019+imina-malak@users.noreply.github.com>
2021-10-19 16:14:15 +0200

    Add files via upload

--- landing-page/README.md
+++ landing-page/README.md
@@ -1,13 +1,22 @@
 # Landing Page Project
 
-## Table of Contents
+# Table of Contents
 
-- [Instructions](#instructions)
+- [Project Title](#Landing-Page-Project)
+- [Table of Contents](#Table-of-Contents)
+- [What the project is](#What-the-project-is)
+- [Development](#Development)
 
-## Instructions
+# What the project is
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+[(Back to top)](#Table-of-contents)
+Landing Page with more than four sections and navigation bar.
+When we click on a navigation item it scrolls smoothly to appropriate section of the page.
+While scrolling through the page, the section that is active in the view will be distinguished.
 
-To get started, open `js/app.js` and start building out the app's functionality
+# Development
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+[(Back to top)](#Table-of-contents)
+build the navigation menu with document Properties and events make it dynamic based on the sections of the page.
+Add function to distinguish the section in view by using classList methods to change the CSS being displayed.
+Add the function to stop the default event when clicking on the navigation item and add a new event to make the scroll smooth.
--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -1,112 +1,70 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-
-/**
- * Define Global Variables
- * 
-*/
-let nav = document.getElementById('navbar__list');
-let sections = document.querySelectorAll('section')
-const frag = document.createDocumentFragment(); 
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-function smoothScroll(a) {
-    //prevent default scroll motion 
-    a.preventDefault();
-    //get the href attribute of every anchor
+//Definition of Global Variables
+let nav = document.getElementById("navbar__list");
+let sections = document.querySelectorAll("section");
+const frag = document.createDocumentFragment();
+//Helper Functions
+function smoothScroll(ele){
+    //prevent default scroll motion
+    ele.preventDefault();
+    //get the href attribute for every anchor
     let href = this.getAttribute("href");
-    //measure the displacement to href anchor  
+    //measure the displacement to href of the anchor
     let offsetTop = document.querySelector(href).offsetTop;
-    //set smooth scroll
+    //set smooth scroll motion
     window.scrollTo({
         top: offsetTop,
         behavior: "smooth"
     });
-    }
-
-
-    window.onscroll = function(){myFunction()} 
-    function myFunction(){
+    };
+//make the motion in the section while scrolling 
+    window.onscroll = function(){ScrollFunction()}
+    function ScrollFunction(){
         for (let index = 1; index <= sections.length; index++){
+            //define the first term to determine the range of sections
             let sec =`section${index}`;
+            //define the second term to determine the range of sections
             let secPlus =`section${index+1}`;
+            //get the id of first term
             let element = document.getElementById(sec);
+            //measure the displacement to id of the first term
             let offsetTop = document.getElementById(sec).offsetTop;
-            if ((index+1)<=sections.length) {
+            //see if the finail term is less or equal to the number of sections
+            if ((index+1)<=sections.length){
+                //measure the displacement to id of the second term
                 let offsetTopPlus = document.getElementById(secPlus).offsetTop;
+                //compare if the scrollTop of the body is between the first and second term
                 if(document.body.scrollTop>=offsetTop && document.body.scrollTop < offsetTopPlus ){
-                    if(element.classList.contains("your-active-class")){}
-                    else {sections.forEach(function remove(ele) {ele.classList.remove('your-active-class')});
-                    document.getElementById(sec).classList.add('your-active-class')}}else{};
+                    /*if yes compare we see if it has an active class or not and if yes don't do anything
+                    and if not add the active class*/
+                    if(element.classList.contains("your-active-class")){
+                    }else {sections.forEach(function remove(ele) {ele.classList.remove('your-active-class');});
+                    document.getElementById(sec).classList.add('your-active-class');};}else{};
             }else{
                 if(document.body.scrollTop>=offsetTop){
                     if(element.classList.contains("your-active-class")){}
-                    else {sections.forEach(function remove(ele) {ele.classList.remove('your-active-class')});
-                    document.getElementById(sec).classList.add('your-active-class')}}else{};
+                    else {sections.forEach(function remove(ele) {ele.classList.remove('your-active-class');});
+                    document.getElementById(sec).classList.add('your-active-class');};}else{};
                 }
-                
             }
         }
 
-
-
-
-for (let index = 1; index <=sections.length; index++) {
+for (let index = 1; index <=sections.length; index++){
+    //create li element
     let liElement = document.createElement('li');
-    let aElement =document.createElement('a');
+    //create anchor element
+    let anchorElement =document.createElement('a');
     // I add (menu__link) class to every anchor
-    aElement.classList.add("menu__link");
-    // then add html to every anchor 
-    aElement.innerHTML = `Section ${index} `;
-    // setting href attribute to every anchor 
-    aElement.setAttribute("href",`#section${index}`);
-    //add click event for every anchor 
-    aElement.addEventListener('click',smoothScroll)
-    // append anchor to li 
-    liElement.append(aElement);
-    // append every li to fragment to increase the performance 
+    anchorElement.classList.add("menu__link");
+    // then add html to every anchor
+    anchorElement.innerHTML = `Section ${index}`;
+    // setting href attribute to every anchor
+    anchorElement.setAttribute("href",`#section${index}`);
+    //add click event for every anchor
+    anchorElement.addEventListener('click',smoothScroll);
+    // append anchor to li
+    liElement.append(anchorElement);
+    // append every li to fragment to increase the performance
     frag.appendChild(liElement);
 }
 // add the fragment to the navigation bar
-nav.appendChild(frag)
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-    
+nav.appendChild(frag);
